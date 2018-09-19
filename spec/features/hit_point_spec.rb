19a3051f127fd37387b604ae809c19fe24d40a60
shweta patil <shwetapatil@shwetas-MacBook-Pro.local>
2018-09-19 14:34:16 +0100

    updated hit_point_spec file

--- spec/features/hit_point_spec.rb
+++ spec/features/hit_point_spec.rb
@@ -1,6 +1,9 @@
 feature 'Hit Point' do 
   scenario "seeing player2's Hit Points" do
-  visit('/play')
-  expect(page).to have_content("Player2's Hit Points: 100")
+    visit('/')
+    fill_in('player1_name', with: "Seb")
+    fill_in('player2_name', with: "Shweta")
+    click_button "Submit"
+    expect(page).to have_content("Shweta's Hit Points: 100")
   end
 end

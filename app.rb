d0e98dd28ba9111d45c2cbf57558353bb28361fb
shweta patil <shwetapatil@shwetas-MacBook-Pro.local>
2018-09-19 14:13:43 +0100

    post redirect sessions

--- app.rb
+++ app.rb
@@ -1,14 +1,23 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
-  post '/players_info' do
-    @player1_name = params[:player1_name]
-    @player2_name = params[:player2_name]
+  get '/play' do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
     erb(:players_info)
+  end
+
+  post '/players_info' do
+    session[:player1] = params[:player1_name]
+    session[:player2] = params[:player2_name]
+    redirect '/play'
   end
 
   run! if app_file == $0
